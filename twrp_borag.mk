36dcd3a421eb6f20080b01ed0eda6a12dbed8417
Fahad Ali Javed <fahadalijaved@gmail.com>
2023-05-15 18:46:39 +0300

    Update twrp_borag.mk

--- twrp_borag.mk
+++ twrp_borag.mk
@@ -4,13 +4,17 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Configure base.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from borag device
 $(call inherit-product, device/motorola/borag/device.mk)

30f03ba5d564b28029bc08c8818641a12175dcb0
Fahad Ali Javed <fahadalijaved@gmail.com>
2023-05-16 15:03:46 +0300

    Update twrp_borag.mk

--- twrp_borag.mk
+++ twrp_borag.mk
@@ -12,9 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Configure emulated_storage.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Inherit from borag device
 $(call inherit-product, device/motorola/borag/device.mk)
 
@@ -30,6 +27,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="borag-user 12 SOVS32.121-21-4 9e5f7 release-keys"
 
 BUILD_FINGERPRINT := motorola/borag_retail/borag:12/SOVS32.121-21-4/9e5f7:user/release-keys
-
-# Hide Reflash TWRP
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.vendor_boot=true
